65c590d54a81c224852fa43cceb95d64062a3bd6
naz <nazz@nazz>
2023-03-11 22:35:45 +0500

    splead

---
+++ script.js
@@ -0,0 +1,58 @@
+// ПЕРВОЕ ЗАДАНИЕ
+
+// const data = ['Ivanov', 'ivan','otdel razrabotki','programmist', 2000];
+
+// function func(){
+//     const [imya, famil, prof, proger, opit] = data;
+//     console.log(imya ,famil,prof,proger,opit);
+// }
+// func()
+
+
+
+//  ВТОРОЕ ЗАДАНИЕ
+
+// const data = ['Ivanov', 'ivan','otdel razrabotki','programmist', 2000];
+// function func() {
+
+//     const [imya, surname, info] = data;
+//     console.log(imya,surname,info);
+// }
+// func()
+
+
+
+// ТРЕТЬЕ ЗАДАНИЕ
+
+// const data = ['Ivanov', 'ivan','otdel razrabotki'];
+
+// function func() {
+//     const [name , surname , department ] = data
+
+//     let position;
+//     if(data[3] !== undefined) {
+//         position == data[3]
+//     }else {
+//         position = 'junior'
+//     }
+
+//     console.log(name,surname,department,position);
+// }
+// func()
+
+
+
+// ЧЕТВЕРТОЕ ЗАДАНИЕ 
+
+
+// const data = {
+//     color:'red',
+//     width:400,
+//     height:500,
+// }
+
+// function func(){
+//     const{color , width , height} = data
+//     console.log(color,width,height);
+// }
+// func()
